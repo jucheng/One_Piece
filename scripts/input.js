9efd883aa3b8cf836e1c73a85279acba44fd550c
JC Chen <847278378@qq.com>
2017-04-12 15:59:00 +0800

    the first commit of the project one_piece

---
+++ scripts/input.js
@@ -0,0 +1,18 @@
+﻿/* 搜索文本框效果 */
+$(function(){
+	$("#inputSearch").focus(function(){
+		  $(this).addClass("focus");
+		  if($(this).val() ==this.defaultValue){  
+			  $(this).val("");           
+		  } 
+	}).blur(function(){
+		 $(this).removeClass("focus");
+		 if ($(this).val() == '') {
+			$(this).val(this.defaultValue);
+		 }
+	}).keyup(function(e){
+		if(e.which == 13){
+			alert('抱歉!暂时查询不到您需要的海贼');
+		}
+	})
+})
